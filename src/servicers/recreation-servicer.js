ab9f93ae033ecf58adc287af5b004549a07153c7
Phillip <phillip@getpixeltag.com>
2023-01-18 19:53:57 -0800

    Testinggg

--- src/servicers/recreation-servicer.js
+++ src/servicers/recreation-servicer.js
@@ -32,7 +32,7 @@ const processScrape = async (event, context, callback) => {
     if(data.found){
         await utilities.emailSites();
     }
-
+    console.log('hello lambda');
     utilities.endSelenium();
 }
 
